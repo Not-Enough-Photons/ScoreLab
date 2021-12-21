2a350db6821095e9cb955d6437530c59f41cb9ef
adamdev <hmullinsk@hotmail.com>
2021-12-20 20:06:03 -0500

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(NEP.Scoreworks.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(NEP.Scoreworks.BuildInfo.Company)]
+[assembly: AssemblyProduct(NEP.Scoreworks.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + NEP.Scoreworks.BuildInfo.Author)]
+[assembly: AssemblyTrademark(NEP.Scoreworks.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(NEP.Scoreworks.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(NEP.Scoreworks.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(NEP.Scoreworks.Main), NEP.Scoreworks.BuildInfo.Name, NEP.Scoreworks.BuildInfo.Version, NEP.Scoreworks.BuildInfo.Author, NEP.Scoreworks.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("Stress Level Zero", "BONEWORKS")]
---
+++ src/Main.cs
@@ -0,0 +1,31 @@
+﻿using MelonLoader;
+
+namespace NEP.Scoreworks
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Scoreworks - Version 3.0"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "Not Enough Photons"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "3.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class Main : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+        }
+
+        public override void OnSceneWasUnloaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasUnloaded(buildIndex, sceneName);
+        }
+    }
+}
